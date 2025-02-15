f18c86fb138afdc15e7a004419a48be447266c9a
Rumpel Stilskin <renegat@renegat0x0.ddns.net>
2025-02-15 18:58:54 +0100

    Added data analyzer

---
+++ dataanalyzer.py
@@ -0,0 +1,257 @@
+"""
+TODO
+ - datanalayze --source-url hackernews = search from particular source
+ - Output formats? (md)?
+ - Maybe it could produce a chart?
+
+Scenario of operation:
+ - What was said about Musk, daily
+  $ --find "Musk" --title --daily
+
+ - 2023-10-26 was DOJ hearing in which he participated, I would like see news
+  $ --find "Musk" --title --date 2023-10-26
+"""
+import argparse
+import time
+import os
+import json
+
+from utils.omnisearch import SingleSymbolEvaluator, EquationEvaluator, OmniSearch
+from utils.alchemysearch import AlchemySymbolEvaluator, AlchemyEquationEvaluator, AlchemySearch
+from sqlalchemy import create_engine, MetaData, Table, select
+
+
+class SearchInterface(object):
+
+    def __init__(self, parser=None, engine=None):
+        self.parser = parser
+        self.start_time = time.time()
+        self.engine = engine
+
+        self.files = []
+
+        self.total_entries = 0
+        self.good_entries = 0
+        self.dead_entries = 0
+
+    def print_entry(self, entry):
+        level = self.parser.get_verbosity_level()
+
+        text = ""
+
+        text = "[{:03d}] {}".format(entry.page_rating_votes, entry.link)
+
+        if self.parser.args.title:
+            text += " " + entry.title
+
+        print(text)
+
+        if self.parser.args.description:
+            description = entry.description
+            if description:
+                print(description)
+
+        if self.parser.args.tags:
+            tags = self.get_tags(entry.id)
+            if tags and tags != "":
+                print(tags)
+
+    def get_tags(self, entry_id):
+        destination_metadata = MetaData()
+        destination_table = Table("usertags", destination_metadata, autoload_with=self.engine)
+
+        stmt = select(destination_table).where(destination_table.c.entry_id == entry_id)
+
+        tags = ""
+
+        with self.engine.connect() as connection:
+            result = connection.execute(stmt)
+            rows = result.fetchall()
+            for row in rows:
+                tags += row.tag + ", "
+
+        return tags
+
+    def read_file(self, afile):
+        text = read_file_contents(afile)
+
+        try:
+            j = json.loads(text)
+
+            if "links" in j:
+                return j["links"]
+            if "sources" in j:
+                return j["sources"]
+
+            return j
+        except Exception as E:
+            print("Could not read file: {}".format(afile))
+
+    def get_time_diff(self):
+        return time.time() - self.start_time
+
+    def print_time_diff(self):
+        elapsed_time_seconds = time.time() - self.start_time
+        elapsed_minutes = int(elapsed_time_seconds // 60)
+        elapsed_seconds = int(elapsed_time_seconds % 60)
+        print(f"Time: {elapsed_minutes}:{elapsed_seconds}")
+
+    def handle_row(self, row):
+        """
+        Row is to be expected a 'dict', eg. row["link"]
+        """
+        link = row.link
+
+        level = self.parser.get_verbosity_level()
+
+        self.print_entry(row)
+
+        self.total_entries += 1
+
+    def summary(self):
+        if self.parser.args.summary:
+            if self.parser.args.verify:
+                print("total:{} good:{} dead:{}".format(self.total_entries, self.good_entries, self.dead_entries))
+            else:
+                print("total:{}".format(self.total_entries))
+
+
+class EntrySymbolEvaluator(SingleSymbolEvaluator):
+    """
+    return 1 if true
+    """
+
+    def __init__(self, entry = None):
+        self.entry = entry
+
+    def evaluate_complex_symbol(self, symbol, condition_data):
+        if condition_data[0] not in self.entry:
+            # print("evaluate_complex_symbol: symbol {} not in entry".format(condition_data[0]))
+            return
+
+        entry_field_value = self.entry[condition_data[0]]
+
+        if condition_data[1] == "==":
+            return entry_field_value == condition_data[2]
+
+        if condition_data[1] == "!=":
+            return entry_field_value != condition_data[2]
+
+        if condition_data[1] == ">":
+            return entry_field_value > condition_data[2]
+
+        if condition_data[1] == "<":
+            return entry_field_value < condition_data[2]
+
+        if condition_data[1] == ">=":
+            return entry_field_value >= condition_data[2]
+
+        if condition_data[1] == "<=":
+            return entry_field_value <= condition_data[2]
+
+        if condition_data[1] == "=":
+            return entry_field_value.find(condition_data[2]) >= 0
+
+        raise IOError("Unsupported operator")
+
+    def evaluate_simple_symbol(self, symbol):
+        """
+        TODO we could check by default if entry link == symbol, or sth
+        """
+        link = ""
+        title = ""
+        description = ""
+
+        if "link" in self.entry:
+            link = self.entry["link"]
+        if "title" in self.entry:
+            title = self.entry["title"]
+        if "description" in self.entry:
+            description = self.entry["description"]
+
+        if link and link.find(symbol) >= 0:
+            return True
+        if title and title.find(symbol) >= 0:
+            return True
+        if description and description.find(symbol) >= 0:
+            return True
+
+        return False
+
+
+class DataAnalyzer(object):
+    def __init__(self, parser):
+        self.parser = parser
+        self.result = None
+        self.engine = None
+
+    def process(self):
+        if self.is_db_scan():
+            self.engine = create_engine("sqlite:///" + self.parser.args.db)
+
+            row_handler = SearchInterface(self.parser, self.engine)
+
+            searcher = AlchemySearch(self.engine,
+                    self.parser.args.search,
+                    row_handler = row_handler,
+                    args=self.parser.args,
+            )
+            searcher.search()
+
+    def is_db_scan(self):
+        if self.parser.args.db:
+            return True
+
+        return False
+
+
+class Parser(object):
+
+    def parse(self):
+        self.parser = argparse.ArgumentParser(description="Data analyzer program")
+        self.parser.add_argument("--dir", help="Directory to be scanned")
+        self.parser.add_argument("--db", help="DB to be scanned")
+        self.parser.add_argument("--search", help="Search, with syntax same as the main program / site.")
+        self.parser.add_argument("--order-by", default="page_rating_votes", help="order by column.")
+        self.parser.add_argument("--asc", action="store_true", help="order ascending")
+        self.parser.add_argument("--desc", action="store_true", help="order descending")
+
+        self.parser.add_argument("--title", action="store_true", help="displays title")
+        self.parser.add_argument("--description", action="store_true", help="displays description")
+        self.parser.add_argument("--tags", action="store_true", help="displays tags")
+
+        self.parser.add_argument("-i", "--ignore-case", action="store_true", help="Ignores case")
+        self.parser.add_argument("-v", "--verbosity", help="Verbosity level")
+        
+        self.args = self.parser.parse_args()
+
+        if self.args.dir:
+            self.dir = self.args.dir
+        else:
+            self.dir = None
+
+        return True
+
+    def get_verbosity_level(self):
+        level = 1
+        if self.args.verbosity:
+            try:
+                level = int(self.args.verbosity)
+            except Exception as E:
+                print(str(E))
+
+        return level
+
+
+def main():
+    p = Parser()
+    if not p.parse():
+        print("Could not parse options")
+        return
+
+    m = DataAnalyzer(p)
+    m.process()
+
+
+if __name__ == "__main__":
+    main()
---
+++ utils/alchemysearch.py
@@ -0,0 +1,133 @@
+from sqlalchemy import and_, or_, not_, func, MetaData, Table, select
+
+from .omnisearch import (
+    SingleSymbolEvaluator,
+    EquationEvaluator,
+    OmniSearch,
+)
+
+
+class AlchemySymbolEvaluator(SingleSymbolEvaluator):
+    """
+    return 1 if true
+    """
+
+    def __init__(self, table, ignore_case = False):
+        self.table = table
+        self.ignore_case = ignore_case
+
+    def evaluate_complex_symbol(self, symbol, condition_data):
+        # TODO make todo check if symbol exists in table?
+
+        if condition_data[1] == "==":
+            if self.ignore_case:
+                column = self.table.c[condition_data[0]]
+                return func.lower(column) == condition_data[2].lower()
+            else:
+                return self.table.c[condition_data[0]] == condition_data[2]
+
+        if condition_data[1] == "!=":
+            if self.ignore_case:
+                column = self.table.c[condition_data[0]]
+                return func.lower(column) != condition_data[2].lower()
+            else:
+                return self.table.c[condition_data[0]] != condition_data[2]
+
+        if condition_data[1] == ">":
+            return self.table.c[condition_data[0]] > condition_data[2]
+
+        if condition_data[1] == "<":
+            return self.table.c[condition_data[0]] < condition_data[2]
+
+        if condition_data[1] == ">=":
+            return self.table.c[condition_data[0]] >= condition_data[2]
+
+        if condition_data[1] == "<=":
+            return self.table.c[condition_data[0]] <= condition_data[2]
+
+        if condition_data[1] == "=":
+            symbol = condition_data[2]
+            if self.ignore_case:
+                return self.table.c[condition_data[0]].ilike(f"%{symbol}%")
+            else:
+                return self.table.c[condition_data[0]].like(f"%{symbol}%")
+
+        raise IOError("Unsupported operator")
+
+    def evaluate_simple_symbol(self, symbol):
+        """
+        TODO we could check by default if entry link == symbol, or sth
+        """
+        if self.ignore_case:
+            return or_(
+                self.table.c["link"].ilike(f"%{symbol}%"),
+                self.table.c["title"].ilike(f"%{symbol}%"),
+                self.table.c["description"].ilike(f"%{symbol}%"),
+            )
+        else:
+            return or_(
+                self.table.c["link"].like(f"%{symbol}%"),
+                self.table.c["title"].like(f"%{symbol}%"),
+                self.table.c["description"].like(f"%{symbol}%"),
+            )
+
+
+class AlchemyEquationEvaluator(EquationEvaluator):
+    def evaluate_function(self, operation_symbol, function, args0, args1):
+        if function == "And":  # & sign
+            return and_(args0, args1)
+        elif function == "Or":  # | sign
+            return or_(args0, args1)
+        elif function == "Not":  # ~ sign
+            return not_(args0)
+        else:
+            raise NotImplementedError("Not implemented function: {}".format(function))
+
+
+class AlchemyRowHandler(object):
+    def handle_row(self, row):
+        pass
+
+
+class AlchemySearch(object):
+    def __init__(self, db, search_term, row_handler=None, args=None):
+        self.db = db
+        self.search_term = search_term
+        self.alchemy_row_handler = row_handler
+
+        self.args = args
+
+    def search(self):
+        destination_metadata = MetaData()
+        destination_table = Table("linkdatamodel", destination_metadata, autoload_with=self.db)
+
+        symbol_evaluator = AlchemySymbolEvaluator(destination_table, self.args.ignore_case)
+        equation_evaluator = AlchemyEquationEvaluator(self.search_term, symbol_evaluator)
+
+        search = OmniSearch(self.search_term, equation_evaluator=equation_evaluator)
+        combined_query_conditions = search.get_combined_query()
+
+        rows = []
+        with self.db.connect() as connection:
+            order_by_column = getattr(destination_table.c, self.args.order_by, None)
+
+            if order_by_column is None:
+                raise AttributeError(f"Invalid order_by column: {self.args.order_by}")
+
+            # Determine sorting order
+            order_by_clause = (
+                order_by_column.asc() if self.args.asc else order_by_column.desc()
+                if self.args.desc else order_by_column.asc()
+            )
+
+            # Use select() for SQLAlchemy Core
+            stmt = select(destination_table).where(combined_query_conditions).order_by(order_by_clause)
+
+            # Execute the query
+            result = connection.execute(stmt)
+            
+            # Fetch all results
+            rows = result.fetchall()
+
+        for row in rows:
+            self.alchemy_row_handler.handle_row(row)
