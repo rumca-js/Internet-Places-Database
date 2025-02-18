7ded9481d35bad5aa1a1790bae4c014583fe4040
X Y Z <rozbujnik@gmail.com>
2025-02-18 15:48:21 +0100

    Tags are obtained by utils now

--- dataanalyzer.py
+++ dataanalyzer.py
@@ -1,15 +1,16 @@
 """
+Provides information about archive
+
+Examples:
+ - What was said about Musk
+  $ --search "title=Musk"
+ - What was said about Musk (title, link, description, etc)
+  $ --search "Musk"
+
 TODO
- - datanalayze --source-url hackernews = search from particular source
  - Output formats? (md)?
  - Maybe it could produce a chart?
 
-Scenario of operation:
- - What was said about Musk, daily
-  $ --find "Musk" --title --daily
-
- - 2023-10-26 was DOJ hearing in which he participated, I would like see news
-  $ --find "Musk" --title --date 2023-10-26
 """
 import argparse
 import time
@@ -18,7 +19,7 @@ import json
 
 from utils.omnisearch import SingleSymbolEvaluator, EquationEvaluator, OmniSearch
 from utils.alchemysearch import AlchemySymbolEvaluator, AlchemyEquationEvaluator, AlchemySearch
-from sqlalchemy import create_engine, MetaData, Table, select
+from sqlalchemy import create_engine
 
 
 class SearchInterface(object):
@@ -52,25 +53,10 @@ class SearchInterface(object):
                 print(description)
 
         if self.parser.args.tags:
-            tags = self.get_tags(entry.id)
+            entry_table = ReflectedEntryTable(self.engine)
+            tags = entry_table.get_tags_string(entry.id)
             if tags and tags != "":
                 print(tags)
-
-    def get_tags(self, entry_id):
-        destination_metadata = MetaData()
-        destination_table = Table("usertags", destination_metadata, autoload_with=self.engine)
-
-        stmt = select(destination_table).where(destination_table.c.entry_id == entry_id)
-
-        tags = ""
-
-        with self.engine.connect() as connection:
-            result = connection.execute(stmt)
-            rows = result.fetchall()
-            for row in rows:
-                tags += row.tag + ", "
-
-        return tags
 
     def read_file(self, afile):
         text = read_file_contents(afile)
