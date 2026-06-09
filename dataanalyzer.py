a8f7a609b4f06b03d6071609fcfe7c96ece8b696
Rumpel Stilskin <rozbujnik@gmial.com>
2026-06-09 22:04:42 +0200

    Removed redundant files

--- dataanalyzer.py
+++ dataanalyzer.py
@@ -18,38 +18,7 @@ import os
 import json
 from sqlalchemy import create_engine
 
-from linkarchivetools import DbAnalyzer
-
-
-class Parser(object):
-
-    def parse(self):
-        self.parser = argparse.ArgumentParser(description="Data analyzer program")
-        self.parser.add_argument("--db", help="DB to be scanned")
-
-        self.parser.add_argument("--search", help="Search, with syntax same as the main program / site.")
-        self.parser.add_argument("--order-by", default="page_rating_votes", help="order by column.")
-        self.parser.add_argument("--asc", action="store_true", help="order ascending")
-        self.parser.add_argument("--desc", action="store_true", help="order descending")
-        self.parser.add_argument("--table", default="linkdatamodel", help="Table name")
-
-        self.parser.add_argument("--title", action="store_true", help="displays title")
-        self.parser.add_argument("--description", action="store_true", help="displays description")
-        self.parser.add_argument("--status", action="store_true", help="displays status")
-        self.parser.add_argument("--tags", action="store_true", help="displays tags")
-        self.parser.add_argument("--social", action="store_true", help="displays social data")
-        self.parser.add_argument("--date-published", action="store_true", help="displays date-published")
-        self.parser.add_argument("--source", action="store_true", help="displays source")
-
-        self.parser.add_argument("--summary", action="store_true", help="displays summary of tables")
-        self.parser.add_argument("--columns", action="store_true", help="displays summary of tables column nmaes")
-
-        self.parser.add_argument("-i", "--ignore-case", action="store_true", help="Ignores case")
-        self.parser.add_argument("-v", "--verbosity", type=int, default=1, help="Verbosity level")
-        
-        self.args = self.parser.parse_args()
-
-        return True
+from linkarchivetools.dbanalyzer import DbAnalyzer, Parser
 
 
 def main():
