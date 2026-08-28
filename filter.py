1a32d3e174c3516b1fb0bd7dac513f9faeb134a2
Rumpel Stilskin <rozbujnik@gmial.com>
2026-08-28 20:14:01 +0200

    internet update

--- filter.py
+++ filter.py
@@ -1,12 +1,13 @@
 import argparse
 
-from linkarchivetools.dbfilter import DbFilter
+from linkarchivetools.dbfilter import DbFilter, parse
 from linkarchivetools.db2json import Db2JSON
 from linkarchivetools.dbanalyzer import DbAnalyzer
 from linkarchivetools.utils.reflected import ReflectedTable, ReflectedGenericTable
 from linkarchivetools.model import DbConnection
 
 
+"""
 def parse():
     parser = argparse.ArgumentParser(description="Data analyzer program")
     parser.add_argument("--db", default='places.db', help="DB to be scanned")
@@ -15,6 +16,7 @@ def parse():
     args = parser.parse_args()
 
     return parser, args
+"""
 
 
 def main():
@@ -23,16 +25,19 @@ def main():
         print("Please specify database")
         return
 
-    output_file = 'internet.db'
+    db = "internet.db"
 
-    print("Filtering")
-    filter = DbFilter(input_db=args.db,output_db=output_file)
-    filter.truncate()
-    filter.close()
-    print("Filtering DONE")
+    thefilter = DbFilter(db=db)
+    if not thefilter.is_valid():
+        print("Filter is not valid")
+        return
 
-    table = ReflectedTable(engine=connection.engine, connection=connection.connection)
-    table.vacuum()
-    table.close()
+    thefilter.truncate_user_tables() # removes users
+    thefilter.truncate_dynamic_data()
+    thefilter.truncate_configuration_tables()
+    thefilter.obfuscate()
+    #thefilter.vacuum()
+
+    thefilter.close()
 
 main()
